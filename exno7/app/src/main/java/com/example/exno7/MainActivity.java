e3669bde46cf273032b50f6c0178523ea6d4ba59
seenuvasan1947 <seenuthiruvpm@gmail.com>
2023-05-20 10:35:45 +0530

    unwanted codes are removed

--- exno7/app/src/main/java/com/example/exno7/MainActivity.java
+++ exno7/app/src/main/java/com/example/exno7/MainActivity.java
@@ -26,13 +26,7 @@ import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
-    // initializing
-    // FusedLocationProviderClient
-    // object
     FusedLocationProviderClient mFusedLocationClient;
-
-    // Initializing other items
-    // from layout file
     TextView latitudeTextView, longitTextView;
     int PERMISSION_ID = 44;
 
@@ -159,67 +153,3 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-//package com.example.exno7;
-//import android.Manifest;
-//import android.app.Activity;
-//import android.os.Bundle;
-//import android.support.v4.app.ActivityCompat;
-//import android.test.mock.MockPackageManager;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.Toast;
-//public class MainActivity extends Activity {
-//    Button btnShowLocation;
-//    private static final int REQUEST_CODE_PERMISSION = 2;
-//    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
-//    // GPSTracker class
-//    GPSTracker gps;
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        try {
-//            if (ActivityCompat.checkSelfPermission(this, mPermission)
-//                    != MockPackageManager.PERMISSION_GRANTED) {
-//                ActivityCompat.requestPermissions(this, new String[]{mPermission},
-//                        REQUEST_CODE_PERMISSION);
-//// If any permission above not allowed by user, this condition will
-//                execute every time, else your else part will work
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        btnShowLocation = (Button) findViewById(R.id.button);
-//        41// show location button click event
-//        btnShowLocation.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View arg0) {
-//// create class object
-//                gps = new GPSTracker(MainActivity.this);
-//// check if GPS enabled
-//                if(gps.canGetLocation()){
-//                    double latitude = gps.getLatitude();
-//                    double longitude = gps.getLongitude();
-//// \n is for new line
-//                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: "
-//                            + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-//                }else{
-//// can't get location
-//// GPS or Network is not enabled
-//// Ask user to enable GPS/network in settings
-//                    gps.showSettingsAlert();
-//                }
-//            }
-//        });
-//    }
-//}
